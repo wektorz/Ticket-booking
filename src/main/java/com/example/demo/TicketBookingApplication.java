ddd10641960067fb2663cd3da1b5c262dfc7bde4
Wiktor Zdrojewski <wiktorzd@wp.pl>
2022-04-29 00:13:12 +0200

    feat: automatic database initialization, tips on running te app in readme

--- src/main/java/com/example/demo/TicketBookingApplication.java
+++ src/main/java/com/example/demo/TicketBookingApplication.java
@@ -1,13 +1,21 @@
 package com.example.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TicketBookingApplication {
+public class TicketBookingApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TicketBookingApplication.class, args);
 	}
 
+
+	// initializes database, disable on production
+	@Override
+	public void run(String... args) throws Exception {
+		String command = "curl -X POST http://localhost:8080/resetDB ";
+		Process process = Runtime.getRuntime().exec(command);
+	}
 }
